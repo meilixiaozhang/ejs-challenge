d1f2adfe8a6b7290477a6661f18a710d2753d690
meilixiaozhang <redpangci@gmail.com>
2020-04-02 17:02:43 +1100

    add about and contact page

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ app.get("/", function(req, res) {
   res.render("home", {startingContent: homeStartingContent});
 })
 
+app.get("/about", function(req, res) {
+  res.render("about", {startingContent: aboutContent});
+})
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {startingContent: contactContent});
+})
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
+
+app.post("/compose", function(req, res) {
+  console.log(req.body.composition)
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
